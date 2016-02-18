493aeb3745e5e7b754212f2d014afacc56888cb7
krnets <cmantheo@gmail.com>
2016-02-18 15:49:16 +1100

    resuming Fogus book

--- fogus.js
+++ fogus.js
@@ -1,50 +1,50 @@
 var _ = require('underscore');
 
 function splat(fun) {
-    return function(array) {
-        return fun.apply(null, array);
-    };
+  return function(array) {
+    return fun.apply(null, array);
+  };
 }
 
 var addArrayElements = splat(function(x, y) {
-    return x + y
+  return x + y
 });
 
 addArrayElements([1, 2]);
 
 function unsplat(fun) {
-    return function() {
-        return fun.call(null, _.toArray(arguments));
-    };
+  return function() {
+    return fun.call(null, _.toArray(arguments));
+  };
 }
 
 var joinElements = unsplat(function(array) {
-    return array.join(' ')
+  return array.join(' ')
 });
 
 joinElements('a', 'b');
 
 function parseAge(age) {
-    if (!_.isString(age)) fail("Expecting a string");
-    var a;
+  if (!_.isString(age)) fail("Expecting a string");
+  var a;
 
-    note("Attempting to parse an age");
-    a = parseInt(age, 10);
+  note("Attempting to parse an age");
+  a = parseInt(age, 10);
 
-    if (!_.isNaN(a)) {
-        warn(age);
-        a = 0;
-    }
+  if (!_.isNaN(a)) {
+    warn(age);
+    a = 0;
+  }
 
-    return a;
+  return a;
 }
 
 function fail(thing) {
-    throw new Error(thing);
+  throw new Error(thing);
 }
 
 function warn(str) {
-    console.log(str + " That doesn't look like a valid age");
+  console.log(str + " That doesn't look like a valid age");
 }
 
 function note() {}
@@ -55,60 +55,60 @@ var letters = ['a', 'b', 'c'];
 letters[1];
 
 function naiveNth(a, index) {
-    return a[index];
+  return a[index];
 }
 
 naiveNth(letters, 1);
 naiveNth({}, 1);
 
 function isIndexed(data) {
-    return _.isArray(data) || _.isString(data);
+  return _.isArray(data) || _.isString(data);
 }
 
 function nth(a, index) {
-    if (!_.isNumber(index)) fail("Expected a number as the index");
-    if (!isIndexed(a)) fail("Not supported on non-indexed type");
-    if ((index < 0) || (index > a.length - 1))
-        fail("Index value is out of bounds");
+  if (!_.isNumber(index)) fail("Expected a number as the index");
+  if (!isIndexed(a)) fail("Not supported on non-indexed type");
+  if ((index < 0) || (index > a.length - 1))
+    fail("Index value is out of bounds");
 
-    return a[index];
+  return a[index];
 }
 
 function second(a) {
-    return nth(a, 1);
+  return nth(a, 1);
 }
 
 function compareLessThanOrEqual(x, y) {
-    if (x < y) return -1;
-    if (y < x) return 1;
-    return 0;
+  if (x < y) return -1;
+  if (y < x) return 1;
+  return 0;
 }
 
 function lessOrEqual(x, y) {
-    return x <= y;
+  return x <= y;
 }
 
 [2, 3, -1, -6, 0, -108, 42, 10].sort(lessOrEqual)
 
 function comparator(pred) {
-    return function(x, y) {
-        if (truthy(pred(x, y)))
-            return -1;
-        else if (truthy(pred(y, x)))
-            return 1;
-        else
-            return 0;
-    }
+  return function(x, y) {
+    if (truthy(pred(x, y)))
+      return -1;
+    else if (truthy(pred(y, x)))
+      return 1;
+    else
+      return 0;
+  }
 }
 
 
 function lameCSV(str) {
-    return _.reduce(str.split("\n"), function(table, row) {
-        table.push(_.map(row.split(","), function(c) {
-            return c.trim()
-        }));
-        return table;
-    }, []);
+  return _.reduce(str.split("\n"), function(table, row) {
+    table.push(_.map(row.split(","), function(c) {
+      return c.trim()
+    }));
+    return table;
+  }, []);
 }
 
 var peopleTable = lameCSV("name,age,hair\nMerble,35,red\nBob,64,blonde");
@@ -117,17 +117,17 @@ peopleTable;
 _.rest(peopleTable).sort();
 
 function selectNames(table) {
-    return _.rest(_.map(table, _.first));
+  return _.rest(_.map(table, _.first));
 }
 
 function selectAges(table) {
-    return _.rest(_.map(table, second));
+  return _.rest(_.map(table, second));
 }
 
 function selectHairColor(table) {
-    return _.rest(_.map(table, function(row) {
-        return nth(row, 2);
-    }));
+  return _.rest(_.map(table, function(row) {
+    return nth(row, 2);
+  }));
 }
 
 var mergeResults = _.zip;
@@ -142,7 +142,7 @@ mergeResults(selectNames(peopleTable), selectAges(peopleTable))
 //
 
 function existy(x) {
-    return x != null;
+  return x != null;
 }
 
 existy(null)
@@ -153,7 +153,7 @@ existy(0)
 existy(false)
 
 function truthy(x) {
-    return (x !== false) && existy(x);
+  return (x !== false) && existy(x);
 }
 
 truthy(false)
@@ -162,23 +162,23 @@ truthy(0)
 truthy('')
 
 function doWhen(cond, action) {
-    if (truthy(cond))
-        return action();
-    else
-        return undefined;
+  if (truthy(cond))
+    return action();
+  else
+    return undefined;
 }
 
 function executeIfHasField(target, name) {
-    return doWhen(existy(target[name]), function() {
-        var result = _.result(target, name);
-        console.log(['The result is', result].join(' '));
-        return result;
-    });
+  return doWhen(existy(target[name]), function() {
+    var result = _.result(target, name);
+    console.log(['The result is', result].join(' '));
+    return result;
+  });
 }
 
 executeIfHasField([1, 2, 3], 'reverse');
 executeIfHasField({
-    foo: 42
+  foo: 42
 }, 'foo');
 executeIfHasField([1, 2, 3], 'notHere');
 
@@ -186,57 +186,57 @@ executeIfHasField([1, 2, 3], 'notHere');
 [null, undefined, 1, 2, false].map(truthy);
 
 var fortytwo = function() {
-    return 42
+  return 42
 }
 fortytwo();
 
 function weirdAdd(n, f) {
-    return n + f()
+  return n + f()
 };
 weirdAdd(42, function() {
-    return 9
+  return 9
 });
 
 _.each(['whiskey', 'tango', 'foxtrot'], function(word) {
-    console.log(word.charAt(0).toUpperCase() + word.substr(1));
+  console.log(word.charAt(0).toUpperCase() + word.substr(1));
 });
 
 var lyrics = [];
 
 for (var bottles = 99; bottles > 0; bottles--) {
-    lyrics.push(bottles + " bottles of beer on the wall");
-    lyrics.push(bottles + " bottles of beer");
-    lyrics.push("Take one down, pass it around");
+  lyrics.push(bottles + " bottles of beer on the wall");
+  lyrics.push(bottles + " bottles of beer");
+  lyrics.push("Take one down, pass it around");
 
-    if (bottles > 1) {
-        lyrics.push((bottles - 1) + " bottles of beer on the wall.");
-    } else {
-        lyrics.push("No more bottles of beer on the wall!");
-    }
+  if (bottles > 1) {
+    lyrics.push((bottles - 1) + " bottles of beer on the wall.");
+  } else {
+    lyrics.push("No more bottles of beer on the wall!");
+  }
 }
 
 function lyricSegment(n) {
-    return _.chain([])
-        .push(n + " bottles of beer on the wall")
-        .push(n + " bottles of beer")
-        .push("Take one down, pass it around")
-        .tap(function(lyrics) {
-            if (n > 1)
-                lyrics.push((n - 1) + " bottles of beer on the wall.");
-            else
-                lyrics.push("No more bottles of beer on the wall!");
-        })
+  return _.chain([])
+    .push(n + " bottles of beer on the wall")
+    .push(n + " bottles of beer")
+    .push("Take one down, pass it around")
+    .tap(function(lyrics) {
+      if (n > 1)
+        lyrics.push((n - 1) + " bottles of beer on the wall.");
+      else
+        lyrics.push("No more bottles of beer on the wall!");
+    })
 
-    .value();
+  .value();
 }
 
 lyricSegment(9);
 
 function song(start, end, lyricGen) {
-    return _.reduce(_.range(start, end, -1),
-        function(acc, n) {
-            return acc.concat(lyricGen(n));
-        }, []);
+  return _.reduce(_.range(start, end, -1),
+    function(acc, n) {
+      return acc.concat(lyricGen(n));
+    }, []);
 }
 
 song(99, 0, lyricSegment);
@@ -246,7 +246,7 @@ function Point2D(x, y) {
   this._y = y;
 }
 
-new Point2D(0,1);
+new Point2D(0, 1);
 
 function Point3D(x, y, z) {
   Point2D.call(this, x, y);
--- fogus2.js
+++ fogus2.js
@@ -3,48 +3,47 @@ var _ = require('underscore');
 var nums = [1, 2, 3, 4, 5];
 
 function doubleAll(array) {
-    return _.map(array, function(n) {
-        return n * 2
-    });
+  return _.map(array, function(n) {
+    return n * 2
+  });
 }
 
 doubleAll(nums);
 
 function average(array) {
-    var sum = _.reduce(array, function(a, b) {
-        return a + b
-    });
-    return sum / _.size(array);
+  var sum = _.reduce(array, function(a, b) {
+    return a + b
+  });
+  return sum / _.size(array);
 }
 
 average(nums);
 
 function onlyEven(array) {
-    return _.filter(array, function(n) {
-        return (n % 2) === 0;
-    })
+  return _.filter(array, function(n) {
+    return (n % 2) === 0;
+  })
 }
 
 onlyEven(nums);
 
 _.map({
-    a: 1,
-    b: 2
+  a: 1,
+  b: 2
 }, _.identity);
 _.map({
-    a: 1,
-    b: 2
+  a: 1,
+  b: 2
 }, function(v, k, coll) {
-    return [k, v, _.keys(coll)];
+  return [k, v, _.keys(coll)];
 });
 
 
 // reduceRight
-
 var nums = [100, 2, 25];
 
 function div(x, y) {
-    return x / y
+  return x / y
 }
 
 _.reduce(nums, div);
@@ -52,23 +51,23 @@ _.reduce(nums, div);
 _.reduceRight(nums, div);
 
 function allOf() {
-    return _.reduceRight(arguments, function(truth, f) {
-        return truth && f();
-    }, false)
+  return _.reduceRight(arguments, function(truth, f) {
+    return truth && f();
+  }, false)
 }
 
 function anyOf() {
-    return _.reduceRight(arguments, function(truth, f) {
-        return truth || f();
-    }, false);
+  return _.reduceRight(arguments, function(truth, f) {
+    return truth || f();
+  }, false);
 }
 
 function T() {
-    return true
+  return true
 }
 
 function F() {
-    return false
+  return false
 }
 
 allOf(T, T);
@@ -82,16 +81,23 @@ _.find(['a', 'b', 3, 'd'], _.isNumber);
 _.reject(['a', 'b', 3, 'd'], _.isNumber);
 
 function complement(pred) {
-    return function() {
-        return !pred.apply(null, _.toArray(arguments));
-    }
+  return function() {
+    return !pred.apply(null, _.toArray(arguments));
+  }
 }
 
-_.filter(['a','b',3,'d'], complement(_.isNumber));
-_.all([1,2,3,4], _.isNumber);
-_.any([1,2,'c',4], _.isString);
+_.filter(['a', 'b', 3, 'd'], complement(_.isNumber));
+_.all([1, 2, 3, 4], _.isNumber);
+_.any([1, 2, 'c', 4], _.isString);
 
-var people = [{ name: "Rick", age: 30 }, { name: "Jaka", age: 24 }];
+var people = [{
+  name: "Rick",
+  age: 30
+}, {
+  name: "Jaka",
+  age: 24
+}];
 
-_.sortBy(people, function(p) { return p.age });
-
+_.sortBy(people, function(p) {
+  return p.age
+});
